525e9c0e48d22ce037d434d52e194f4c8e13eda7
James Fidler <james.fidler@immediate.co.uk>
2017-01-04 14:39:07 +0000

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app top-navbar>
+    <header>
+      <v-navbar></v-navbar>
+    </header>
+    <main>
+      <v-content>
+        <v-container>
+          <v-card class="green">
+            <v-card-text>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam animi atque cum cumque dignissimos doloremque eum, explicabo id iusto magnam nemo neque nisi obcaecati quidem quod ratione repellat temporibus!
+            </v-card-text>
+          </v-card>
+        </v-container>
+      </v-content>
+    </main>
+  </v-app>
+</template>
+
+<script>
+import Hello from './components/Hello';
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
